7199188aa2fb3eee29615e6fbc31b8307817e48d
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-01 10:21:12 +0530

    priority-based-collections

---
+++ MoreCSharp/CollectionExecutables/GenericPriorityBased.cs
@@ -0,0 +1,21 @@
+﻿using MoreCSharp.Delegates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreCSharp.CollectionExecutables
+{
+    internal class GenericPriorityBased
+    {
+        static void Main(string[] q) {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
---
+++ MoreCSharp/CollectionExecutables/PriorityBased.cs
@@ -0,0 +1,47 @@
+﻿using MoreCSharp.Generics;
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace MoreCSharp.CollectionExecutables
+{
+    internal class PriorityBased
+    {
+        static void Main(string[] b)
+        {
+            Stack stack = new Stack();
+            stack.Push(1);
+            stack.Push("Raju");
+            stack.Push(true);
+            stack.Push(new Store(5));
+            stack.Push(new Stack());
+
+            Console.WriteLine(stack);
+            foreach(var item in stack)
+                Console.WriteLine(item);
+            stack.Pop();
+            stack.Pop();
+
+            foreach (var item in stack)
+                Console.WriteLine(item);
+
+            Queue queue= new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue("Roger");
+            queue.Enqueue("Christopher");
+
+            foreach (var item in queue) {
+                Console.WriteLine(item);
+            }
+            queue.Dequeue();
+            queue.Dequeue();
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
